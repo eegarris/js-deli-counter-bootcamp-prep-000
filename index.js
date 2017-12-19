9f6b3ebb1a478f3b23c6e06849a1bbad541fcd1d
Emily Garrison <emily.e.garrison@gmail.com>
2017-12-19 05:06:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ function takeANumber(katzDeliLine, name) {
   katzDeliLine.push(name);
   return `Welcome, ${name}. You are number ${katzDeliLine.length} in line.`;
 }
+
+function nowServing(katzDeliLine) {
+  if (katzDeliLine.length > 0) {
+    return 
+  }
+}
