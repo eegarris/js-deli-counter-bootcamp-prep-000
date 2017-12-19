4840b096d87f77ef81078d8c2009352d083fb529
Emily Garrison <emily.e.garrison@gmail.com>
2017-12-19 05:41:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function currentLine(katzDeliLine) {
     return "The line is currently empty."
   }
   else {
-    for (var i = 0, i < katzDeliLine.length - 1, i++) {
+    for (var i = 0; i < katzDeliLine.length - 1; i++) {
      var line = " ";
      line += (i + 1) + ". " + katzDeliLine[i] + ", ";
     }
