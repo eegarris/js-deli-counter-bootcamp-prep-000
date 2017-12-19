b9c8c697a6b67434ae064d3905639924a426ffce
Emily Garrison <emily.e.garrison@gmail.com>
2017-12-19 05:12:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,3 +14,12 @@ function nowServing(katzDeliLine) {
     return "There is nobody waiting to be served!";
   }
 }
+
+function currentLine(katzDeliLine) {
+  if (katzDeliLine.length === 0) {
+    return "The line is currently empty."
+  }
+  else {
+    return `The line is currently
+  }
+}
